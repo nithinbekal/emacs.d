413029cc57eda819f02f1a483320bef97c313277
Nithin Bekal <nithinbekal@gmail.com>
2016-07-12 19:52:36 +0530

    Setup helm

--- init.el
+++ init.el
@@ -16,6 +16,7 @@
 (defvar packages-list
   '(
     evil
+    helm
     ujelly-theme
     )
   "List of packages that are installed on first startup.")
@@ -35,4 +36,17 @@
 ; Enable evil-mode
 (require 'evil)
 (evil-mode 1)
+
+; Helm allows fuzzy autocomplete for interactions requiring selecting an item
+; from many possible choices.
+; Detailed tutorial: http://tuhdo.github.io/helm-intro.html
+
+; Tramp sometimes messes with helm while it tries to figure out SSH/DNS
+; settings. This will avoid the issues.
+(setq tramp-ssh-controlmaster-options
+      "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
+
+(require 'helm)
+(require 'helm-config)
+(helm-mode 1)
 
