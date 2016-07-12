a97349230a469132cfe91c67af9e156f8778e33f
Nithin Bekal <nithinbekal@gmail.com>
2016-07-13 00:39:25 +0530

    Switch to use-package for loading packages

--- init.el
+++ init.el
@@ -1,73 +1,73 @@
 
-
 ; package.el is the built-in package manager included in emacs.
 ; By default it uses the GNU ELPA repository to look for packages.
 ; GNU ELPA contains a very small number of packages, so we add MELPA, where
 ; most of the popular packages are available.
 
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")))
 
-; Add all the packages you want to install in the packages-list variable.
-; When emacs starts, package.el will look for packages that are not installed
-; yet and install them.
 
-(defvar packages-list
-  '(
-    evil
-    evil-leader
-    helm
-    ujelly-theme
-    )
-  "List of packages that are installed on first startup.")
+; UI related stuff
 
-(dolist (p packages-list)
-  (unless (package-installed-p p)
-    (package-install p)))
-
-; Enable ujelly-theme - I use jellybean theme with vim, and ujelly is the
-; closest one I've found for emacs.
-
-(require 'ujelly-theme)
-
-; Disable the menu bar at the top.
 (menu-bar-mode -1)
 
 
-; evil-mode
-; ****************
+; use-package allows you to load packages lazily, and speeds up the initial
+; load time of emacs.
 
-(require 'evil)
-(global-evil-leader-mode)
-(evil-mode 1)
+(unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
 
-(require 'evil-leader)
-
-; Define leader key mappings
-
-(evil-leader/set-leader ",")
-
-(evil-leader/set-key
-  "t"  'helm-find-files
-  "v"  'split-window-right
-  "yb" (kbd "gg v G y")     ; Yank buffer
-  )
+(require 'use-package)
 
 
-; helm-mode
-; *********
+(use-package evil
+  ; Powerfule vim emulation inside emacs.
 
-; Helm allows fuzzy autocomplete for interactions requiring selecting an item
-; from many possible choices.
-; Detailed tutorial: http://tuhdo.github.io/helm-intro.html
+  :ensure t
+  :init
+  (progn
 
-; Tramp sometimes messes with helm while it tries to figure out SSH/DNS
-; settings. This will avoid the issues.
-(setq tramp-ssh-controlmaster-options
-      "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
+    (use-package evil-leader
+      ; Configure leader keys for evil mode.
 
-(require 'helm)
-(require 'helm-config)
-(helm-mode 1)
+      :ensure t
+      :init (global-evil-leader-mode)
+
+      :config
+      (progn
+        (evil-leader/set-leader ",")
+        (evil-leader/set-key
+          "t"  'helm-find-files
+          "v"  'split-window-right
+          "yb" (kbd "gg v G y")     ; Yank buffer
+        )))
+    (evil-mode 1)))
+
+
+(use-package helm
+  ; Helm allows fuzzy autocomplete for interactions requiring selecting an item
+  ; from many possible choices.
+  ; Detailed tutorial: http://tuhdo.github.io/helm-intro.html
+
+  :ensure t
+  :init
+  (progn
+    ; Tramp sometimes messes with helm while it tries to figure out SSH/DNS
+    ; settings. This will avoid the issues.
+    (setq tramp-ssh-controlmaster-options
+          "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
+
+    (require 'helm-config)
+    (helm-mode)))
+
+
+(use-package ujelly-theme
+  ; Enable ujelly-theme - I use jellybean theme with vim, and ujelly is the
+  ; closest one I've found for emacs.
+  :ensure t)
 
