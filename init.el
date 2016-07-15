cfa24ce1d4beac53cecdc65e15409502f47b058f
Nithin Bekal <nithinbekal@gmail.com>
2016-07-15 19:11:50 +0530

    Sort evil leader keybindings alphabetically

--- init.el
+++ init.el
@@ -109,17 +109,6 @@
       "dd" 'dash-at-point
       "f"  'helm-projectile-ag
       "gs" 'magit-status
-      "q"  'kill-buffer-and-window
-      "s"  'projectile-toggle-between-implementation-and-test
-      "vs"  'split-window-right
-
-      "vi" (lambda ()
-             (interactive)
-             (find-file "~/.emacs.d/init.el"))
-
-      "yb" (kbd "gg v G y")     ; Yank buffer
-      "yt" 'yafolding-toggle-element
-      "yy" 'yafolding-toggle-all
 
       "gg" (lambda ()
              (interactive)
@@ -128,6 +117,18 @@
       "gt" (lambda ()
              (interactive)
              (find-file "~/Dropbox/todo/gtd.md"))
+
+      "q"  'kill-buffer-and-window
+      "s"  'projectile-toggle-between-implementation-and-test
+      "vs"  'split-window-right
+
+      "vi" (lambda ()
+             (interactive)
+             (find-file "~/.emacs.d/init.el"))
+
+      "yb" (kbd "gg v G y") ; Yank buffer
+      "yt" 'yafolding-toggle-element
+      "yy" 'yafolding-toggle-all
       )
 
     :ensure t)
