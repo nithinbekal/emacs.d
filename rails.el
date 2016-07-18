c7ae7756e654ac594cc74c79de2c0a3671de6076
Nithin Bekal <nithinbekal@gmail.com>
2016-07-18 12:42:45 +0530

    Add rails startup script

---
+++ rails.el
@@ -0,0 +1,11 @@
+
+;; Emacs lisp script to start rails server in the background from within emacs.
+;; Usage:
+;; Run the following command from the root of your rails project.
+;;   $ emacs --load ~/.emacs.d/rails.el
+
+(interactive)
+(pop-to-buffer (get-buffer-create (generate-new-buffer-name "rails-server")))
+(shell (current-buffer))
+(process-send-string nil "bundle exec rails s\n")
+(delete-window)
