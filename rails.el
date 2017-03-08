bd7268fdf45f82dfa5f73db09364200d0bcc2ca0
Nithin Bekal <nithinbekal@gmail.com>
2017-03-09 01:26:46 +0530

    Update rails.el script

--- rails.el
+++ rails.el
@@ -5,7 +5,13 @@
 ;;   $ emacs --load ~/.emacs.d/rails.el
 
 (interactive)
+
 (pop-to-buffer (get-buffer-create (generate-new-buffer-name "rails-server")))
 (shell (current-buffer))
 (process-send-string nil "bundle exec rails s\n")
+(delete-window)
+
+(pop-to-buffer (get-buffer-create (generate-new-buffer-name "rails-console")))
+(shell (current-buffer))
+(process-send-string nil "bundle exec rails console\n")
 (delete-window)
